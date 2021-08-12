ae5e20ce9a2be8ef61ece37bcc8b79c27ce404cd
Betcyp <88311489+Betcyp@users.noreply.github.com>
2021-08-12 19:22:39 +0530

    Update MainQueue.java

--- MasterWorkerTask/src/com/user/MainQueue.java
+++ MasterWorkerTask/src/com/user/MainQueue.java
@@ -8,10 +8,15 @@ public class MainQueue {
     {
         Queue<Job> queue =new LinkedList<Job>();
 		Job j=new Job(3);
+	    	Job j1=new Job(4)
 		System.out.println("adding element: "+j.getSquare());
+	    	System.out.println("adding element: "+j1.getSquare());
 		queue.add(j);
-		Job removedJob=queue.remove();
-		System.out.println("removed element: "+removedJob.getSquare());
+	    	queue.add(j1);
+	    	System.out.println("before removing: "+queue.size());
+	    	Job removedJob=queue.remove();
+	    	System.out.println("removed element is: "+removedJob.getSquare());
+	    	System.out.println("After removing: "+queue.size());
     }
 }
 
